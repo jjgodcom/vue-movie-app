f2df8685c39ab4fdd7dd32d6d7b4e6599eab8681
jjgodcom <jjgodcom1@gmail.com>
2022-09-24 03:18:23 +0900

    Header - Logo 추가 구글폰트 세팅

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <Logo></Logo>
     <div class="nav nav-pills">
       <div 
         v-for="nav in navigations"
@@ -20,7 +21,12 @@
 </template>
 
 <script>
+import Logo from '~/components/Logo'
+
 export default {
+  components:{
+    Logo
+  },
   data(){
     return{
       navigations: [
@@ -41,3 +47,15 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+header {
+  height: 70px;
+  padding: 0 40px;
+  display: flex;
+  align-items: center;
+  .logo{
+    margin-right: 40px;
+  }
+}
+</style>
