e120982bf12928461f118259a52b234e8f755bb5
jjgodcom <jjgodcom1@gmail.com>
2022-09-23 23:35:57 +0900

    경로 오류 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHashHistory} from 'vue-router'
 import Home from './Home'
 import About from './About'
 
@@ -7,7 +7,7 @@ export default createRouter({
   // Hash는 해당하는 페이지에 /#/을 붙여서 접근하는 모드
   // 특정페이지에서 새로고침시 404방지
   // History 모드는 따로 서버에 세팅이 필요하기때문에 이 프로젝트에서는 Hash 모드 사용
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   // routes 는 배열 데이터로 만들기
   // routes 는 페이지를 구분해주는 개념
   routes: [
