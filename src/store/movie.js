22d32db6ce69213377a0b2b8a27ecb005606ee2b
jjgodcom <jjgodcom1@gmail.com>
2022-09-27 17:54:46 +0900

    영화 검색 추가 요청

--- src/store/movie.js
+++ src/store/movie.js
@@ -30,7 +30,7 @@ export default {
   },
   // 비동기로 동작
   actions:{
-    async searchMovies({commit}, payload){
+    async searchMovies({state,commit}, payload){
       const {title, type, number, year} = payload;
       const OMDB_API_KEY = '7035c60c';
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`);
@@ -38,6 +38,21 @@ export default {
       commit('updateState', {
         movies: Search
       })
+      // totalResults : 숫자로 나오지만 문자데이터
+      const total = parseInt(totalResults, 10);
+      const pageLength = Math.ceil(total / 10);
+
+      // 추가 요청 전송
+      if(pageLength > 1){
+        for(let page = 2; page<= pageLength; page += 1){
+          if(page > (number/10))break
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`);
+          const {Search} = res.data;
+          commit('updateState', {
+            movies: [...state.movies , ...Search]
+          })
+        }
+      }
     }
   }
 }
